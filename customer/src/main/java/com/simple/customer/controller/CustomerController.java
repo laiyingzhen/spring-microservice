e41749e9f3e3ec9de58f060418958d6de0d2b39f
laiyingzhen <jasmine.jen@gmail.com>
2022-08-11 06:18:30 +0800

    remove failed implementation

--- customer/src/main/java/com/simple/customer/controller/CustomerController.java
+++ customer/src/main/java/com/simple/customer/controller/CustomerController.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
